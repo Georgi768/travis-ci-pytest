cc0c9c7b7e2da35c63f9abe67037ec88f8522411
Georgi768 <59054341+Georgi768@users.noreply.github.com>
2021-04-30 12:31:08 +0200

    checking changes

--- setup.py
+++ setup.py
@@ -13,4 +13,4 @@ setup(name='Fibonacci',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-     )
+      )
